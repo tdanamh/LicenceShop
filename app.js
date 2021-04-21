bc3a3a758016bb8eb6cbfa1090cf52c6bd6bd989
dana <tdanamh@gmail.com>
2021-04-21 13:05:06 +0300

    Created user api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,15 +14,15 @@ var app = express();
 const db = require('./config/keys').mongoURI;
 // Connect to Database
 mongoose.connect(db,  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected."))
-  .catch(err => console.log(err));
-
+.then(() => console.log("MongoDB connected."))
+.catch(err => console.log(err));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
