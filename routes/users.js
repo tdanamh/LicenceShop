0604e629cbd85f874b833082e967ac2bc2415884
dana <tdanamh@gmail.com>
2021-04-21 13:31:27 +0300

    Created user login api

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jwtKey = require('../config/keys').JWT_KEY;
 
 const router = express.Router();
 const saltRounds = 10;
@@ -50,7 +52,7 @@ router.put('/', function(req, res) {
   User.find({ email: email })
   .exec()
   .then(user => {
-    if (user.length == 0) {
+    if (user.length === 0) {
       return res.status(404).json({ message: 'Cont inexistent!' });
     }
     bcrypt.compare(password, user[0].password, function(err, result) {
@@ -83,5 +85,35 @@ router.delete('/:id', function(req, res) {
   })
   .catch(err => res.status(500).json({ error: err }));
 })
+
+/* POST -> login using email && password */
+router.post('/login', function(req, res) {
+  let email = req.body.email;
+  let password = req.body.password;
+  User.find({ email: email })
+  .exec()
+  .then(user => {
+    if (user.length === 0) {
+      return res.status(404).json({ message: 'Cont inexistent!' });
+    }
+    if (user[0].isAdmin === true) {
+      return res.status(401).json({ message: 'Credentiale gresite!' });
+    }
+    bcrypt.compare(password, user[0].password, function(err, result) {
+      if (result === false) {
+        return res.status(401).json({ message: 'Credentiale gresite!' });
+      }
+      let token = jwt.sign({
+        userId: user[0]._id,
+        email: user[0].email,
+        firstName: user[0].firstName,
+        lastName: user[0].lastName,
+        isAdmin: user[0].isAdmin,
+      }, jwtKey, { expiresIn: "1h" });
+      return res.status(202).json({ token: token, message: 'Autentificare reusita!' });
+    });
+  })
+  .catch(err => res.status(500).json({ error: err }));
+});
 
 module.exports = router;
