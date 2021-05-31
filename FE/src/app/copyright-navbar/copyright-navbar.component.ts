c03da470123d207b067fa3517c835b273c73af39
dana <tdanamh@gmail.com>
2021-06-01 00:31:24 +0300

    Restyle homepage

---
+++ FE/src/app/copyright-navbar/copyright-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-copyright-navbar',
+  templateUrl: './copyright-navbar.component.html',
+  styleUrls: ['./copyright-navbar.component.css']
+})
+export class CopyrightNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
